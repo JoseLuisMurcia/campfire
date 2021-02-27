1e09f3cee05e71b0a27e944e34678df55aab0e90
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2021-02-27 12:30:24 +0100

    Getters y setters en avatar

--- src/main/java/es/urjc/etsii/dad/campfire/model/Avatar.java
+++ src/main/java/es/urjc/etsii/dad/campfire/model/Avatar.java
@@ -1,13 +1,20 @@
 package es.urjc.etsii.dad.campfire.model;
 import java.awt.Color;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Avatar
 {
-    public Color primaryColor;
-    public Color secondaryColor;
-    public Color eyesColor;
-    public int hatId;
-    public int accessoryId;
+    private Color primaryColor;
+    private Color secondaryColor;
+    private Color eyesColor;
+    private int hatId;
+    private int accessoryId;
+    //Primary key
+    @OneToOne
+    private User user;
 
     public Avatar(Color pColor, Color sColor, Color eColor, int hatId, int acessoryId)
     {
@@ -16,5 +23,53 @@ public class Avatar
         this.eyesColor = eColor;
         this.hatId = hatId;
         this.accessoryId = acessoryId;
+    }
+
+    public Color getPrimaryColor() {
+        return primaryColor;
+    }
+
+    public void setPrimaryColor(Color primaryColor) {
+        this.primaryColor = primaryColor;
+    }
+
+    public Color getSecondaryColor() {
+        return secondaryColor;
+    }
+
+    public void setSecondaryColor(Color secondaryColor) {
+        this.secondaryColor = secondaryColor;
+    }
+
+    public Color getEyesColor() {
+        return eyesColor;
+    }
+
+    public void setEyesColor(Color eyesColor) {
+        this.eyesColor = eyesColor;
+    }
+
+    public int getHatId() {
+        return hatId;
+    }
+
+    public void setHatId(int hatId) {
+        this.hatId = hatId;
+    }
+
+    public int getAccessoryId() {
+        return accessoryId;
+    }
+
+    public void setAccessoryId(int accessoryId) {
+        this.accessoryId = accessoryId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
