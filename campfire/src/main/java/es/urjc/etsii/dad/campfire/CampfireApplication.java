d3cc20b474cb4d00b4f68e79b626c89b40070f6f
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2021-04-08 14:03:38 +0200

    Internal service correctly connected to the chat

--- campfire/src/main/java/es/urjc/etsii/dad/campfire/CampfireApplication.java
+++ campfire/src/main/java/es/urjc/etsii/dad/campfire/CampfireApplication.java
@@ -19,12 +19,6 @@ public class CampfireApplication implements WebSocketConfigurer {
 
 	public static void main(String[] args) throws TTransportException {
 		SpringApplication.run(CampfireApplication.class, args);
-
-		CrossPlatformServiceClient c1 = new CrossPlatformServiceClient();
-		System.out.println("FILTER RESULT: " + c1.sendMessage("Chúpamela polla"));
-		System.out.println("FILTER RESULT: " + c1.sendMessage("Chúpame la polla"));
-		System.out.println("FILTER RESULT: " + c1.sendMessage("maricón"));
-		System.out.println("FILTER RESULT: " + c1.sendMessage("marica"));
 	}
 
 	@Override
