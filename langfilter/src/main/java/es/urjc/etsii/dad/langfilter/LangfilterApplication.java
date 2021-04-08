6bf12c93666933e833cb129d0571edae43074953
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2021-04-08 12:20:01 +0200

    Solucionando conflictos con carpetas

--- langfilter/src/main/java/es/urjc/etsii/dad/langfilter/LangfilterApplication.java
+++ langfilter/src/main/java/es/urjc/etsii/dad/langfilter/LangfilterApplication.java
@@ -1,4 +1,4 @@
-package es.urjc.etsii.dad.langfilter.langfilter;
+package es.urjc.etsii.dad.langfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
